43446080b61777886d23e3910da5d55f78496b0a
YaoHui <teaoea@gmail.com>
2021-07-11 22:08:01 +0800

    feature: email component

---
+++ src/app/ui/form/email/email.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'email',
+  templateUrl: './email.component.html',
+  styleUrls: ['./email.component.scss']
+})
+export class EmailComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  value: string = '';
+
+  @Output() email: EventEmitter<string> = new EventEmitter();
+
+  newEmail(value: string) {
+    this.email.emit(value)
+  }
+}
