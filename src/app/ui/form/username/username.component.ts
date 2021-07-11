feaa03ac686586e5e7cbe4349a87a8d73ce6b151
YaoHui <teaoea@gmail.com>
2021-07-12 06:52:45 +0800

    chore: add validators and use it

--- src/app/ui/form/username/username.component.ts
+++ src/app/ui/form/username/username.component.ts
@@ -14,16 +14,28 @@ export class UsernameComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  formControl = new FormControl('', [
-    Validators.required, Validators.pattern(/^[a-zA-Z]/),
-    Validators.minLength(5), Validators.maxLength(32)
-  ]);
-
   value: string = '';
 
   @Output() username: EventEmitter<string> = new EventEmitter();
 
   newUsername(value: string) {
     this.username.emit(value);
+  }
+
+  formControl = new FormControl('', [
+    Validators.required, Validators.pattern(/^[a-zA-Z_-]{5,32}$/),
+    Validators.minLength(5), Validators.maxLength(32)
+  ]);
+
+  errorMessage(): any {
+    if (this.formControl.hasError('required')) {
+      return 'You must enter a value';
+    } else if (this.formControl.hasError('pattern')) {
+      return 'Allow of letters,underscores and horizontal lines';
+    } else if (this.formControl.hasError('minLength')) {
+      return 'At least 5 characters';
+    } else if (this.formControl.hasError('maxLength')) {
+      return 'Up to 32 characters';
+    }
   }
 }
