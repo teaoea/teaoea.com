16794433f3af1a989beaf7be568584f29e6df911
YaoHui <teaoea@gmail.com>
2021-07-12 08:40:51 +0800

    chore: add validators and use it

--- src/app/ui/form/confirm-password/confirm-password.component.ts
+++ src/app/ui/form/confirm-password/confirm-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'confirm-password',
@@ -28,6 +29,37 @@ export class ConfirmPasswordComponent implements OnInit {
   @Output() password2: EventEmitter<string> = new EventEmitter();
 
   newPassword2(value: string) {
-    this.password2.emit(value)
+    this.password2.emit(value);
   }
+
+  formControl = new FormControl('', [
+    Validators.required, Validators.minLength(8), Validators.maxLength(32)
+  ]);
+
+  errorMessage(): any {
+    if (this.formControl.hasError('required')) {
+      return 'You must enter a value';
+    } else if (this.formControl.hasError('minLength')) {
+      return 'At least 8 characters';
+    } else if (this.formControl.hasError('maxLength')) {
+      return 'Up to 32 characters';
+    }
+  }
+
+  formControl2 = new FormControl('', [
+    Validators.required, Validators.minLength(8), Validators.maxLength(32)
+  ]);
+
+  errorMessage2(): any {
+    if (this.formControl.hasError('required')) {
+      return 'You must enter a value';
+    } else if (this.formControl.hasError('minLength')) {
+      return 'At least 8 characters';
+    } else if (this.formControl.hasError('maxLength')) {
+      return 'Up to 32 characters';
+    } else if (this.value !== this.value2) {
+      return 'Inconsistent passwords';
+    }
+  }
+
 }
