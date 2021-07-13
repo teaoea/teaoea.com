1f5a7c6cd3f85d065ff88bbafd3a9161707e1b1b
YaoHui <teaoea@gmail.com>
2021-07-13 15:51:00 +0800

    chore: Routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './ui/home/home.component';
 import { SignupComponent } from './user/signup/signup.component';
 import { SigninComponent } from './user/signin/signin.component';
-import { SigninGuard } from './tools/signin.guard';
 import { MeComponent } from './user/me/me.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'account/signup', component: SignupComponent},
-  {path: 'account/signin', component: SigninComponent},
   {
-    path: 'account', canActivate: [SigninGuard], children: [
+    path: 'account', children: [
+      {path: 'signup', component: SignupComponent},
+      {path: 'signin', component: SigninComponent},
       {path: 'me', component: MeComponent}
     ]
   }
