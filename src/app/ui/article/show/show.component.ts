c67b7ccc7222406dc4b85c14082726e01f23d3d7
YaoHui <teaoea@gmail.com>
2021-07-19 09:38:10 +0800

    feature: to-show component

---
+++ src/app/ui/article/show/show.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { show } from './show';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'to-show',
+  templateUrl: './show.component.html',
+  styleUrls: ['./show.component.scss']
+})
+export class ShowComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  s = show;
+
+  value: string = '';
+
+  @Output() show: EventEmitter<boolean> = new EventEmitter;
+
+  newShow(value: string) {
+    this.show.emit(Boolean(value));
+  }
+
+  formControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  errorMessage(): any {
+    if (this.formControl.hasError('required')) {
+      return 'Must choose a category'
+    }
+  }
+}
---
+++ src/app/ui/article/show/show.ts
@@ -0,0 +1,15 @@
+export interface Show {
+  key: string
+  value: boolean
+}
+
+export const show: Show[] = [
+  {
+    key: 'public',
+    value: true
+  },
+  {
+    key: 'private',
+    value: true
+  }
+];
