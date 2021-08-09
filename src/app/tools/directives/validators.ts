3748a8d5c6bc75e41da1a4e00a4650b56bba35e5
YaoHui <teaoea@gmail.com>
2021-08-09 21:49:06 +0800

    chore: Check email address suffix

--- src/app/tools/directives/validators.ts
+++ src/app/tools/directives/validators.ts
@@ -1,28 +1,15 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 import config from '../../../config.json';
 
-export class BaseValidator {
-
-
-  constructor() {
-  }
-
-  /**
-   * @description check whether the email suffix can be used for sign up
-   * @param email email address
-   * @returns true ok
-   */
-  static suffixes(email: string): boolean {
-    const suffix = '@' + email.split('@')[1];
-    const suffixes = config.suffixes;
-    const index = suffixes.indexOf(suffix);
-    return index != -1;
-  }
-
+function suffix(email: string): boolean {
+  const suffix = '@' + email.split('@')[1];
+  const suffixes = config.suffixes;
+  const index = suffixes.indexOf(suffix);
+  return index != -1;
 }
 
 export function forbiddenEmailSuffixes(control: AbstractControl): ValidationErrors | null {
-  if (!BaseValidator.suffixes(control.value)) {
+  if (!suffix(control.value)) {
     return {'suffix': true};
   }
   return null;
