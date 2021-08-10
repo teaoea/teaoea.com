c095f9317f9cdae04aff13854b37955ac08db2e2
YaoHui <teaoea@gmail.com>
2021-08-10 20:44:12 +0800

    chore: Modify the translation file

--- src/app/ui/user/phone/country.ts
+++ src/app/ui/user/phone/country.ts
@@ -5,15 +5,15 @@ export interface Country {
 
 export const countries: Country[] = [
   {
-    key: $localize`:@@6299596513869757905:United States +1`,
+    key: $localize`:@@6299596513869757905:🇺🇸 United States +1`,
     value: '+1'
   },
   {
-    key: $localize`:@@4217447493960184043:China +86`,
+    key: $localize`:@@4217447493960184043:🇨🇳 China +86`,
     value: '+86'
   },
   {
-    key: $localize`:@@4440942506526994150:Hong Kong,China +852`,
+    key: $localize`:@@4440942506526994150:🇭🇰 Hong Kong,China +852`,
     value: '+852'
   }
 ];
