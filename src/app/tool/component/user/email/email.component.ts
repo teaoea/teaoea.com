18ee0d8f75e9ddd4ed1a3b1a91954a4e996b6d19
YaoHui <teaoea@gmail.com>
2023-01-06 08:11:05 +0800

    🌈 style: 统一设计

--- src/app/tool/component/user/email/email.component.ts
+++ src/app/tool/component/user/email/email.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-email',
-  templateUrl: './email.component.html',
-  styleUrls: ['./email.component.scss']
+  selector: "app-email",
+  templateUrl: "./email.component.html",
+  styleUrls: ["./email.component.scss"],
 })
 export class EmailComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  value: string = "";
 
-  ngOnInit(): void {
+  @Output() email: EventEmitter<string> = new EventEmitter();
+
+  newEmail(value: string) {
+    this.email.emit(value);
   }
 
+  formControl = new FormControl(this.value, [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(50),
+  ]);
 }
