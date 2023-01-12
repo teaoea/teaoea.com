af08bca003150baeac20fd9ca204ebbd73542785
YaoHui <teaoea@gmail.com>
2023-01-12 23:20:03 +0800

    style: Modify notification window style

--- src/app/user/signup/signup.service.ts
+++ src/app/user/signup/signup.service.ts
@@ -15,7 +15,8 @@ export class SignupService {
     private router: Router,
     private route: ActivatedRoute,
     private snackBar: MatSnackBar
-  ) {}
+  ) {
+  }
 
   post(signup: Signup) {
     return this.http
@@ -23,30 +24,50 @@ export class SignupService {
       .subscribe({
         next: (response) => {
           localStorage.setItem("Authorization", response.body.message);
-          this.router.navigate(["/"], { relativeTo: this.route }).then();
+          this.router.navigate(["/"], {relativeTo: this.route}).then();
         },
         error: (error) => {
           switch (error.status) {
             case 460:
-              this.snackBar.open("两次输入的密码不一致");
+              this.snackBar.open("两次输入的密码不一致", "", {
+                duration: 1500,
+                panelClass: 'snackBar'
+              });
               break;
             case 461:
-              this.snackBar.open("密码不够安全,请使用更安全的密码");
+              this.snackBar.open("密码不够安全,请使用更安全的密码", "", {
+                duration: 1500,
+                panelClass: 'snackBar'
+              });
               break;
             case 462:
-              this.snackBar.open("用户名已被注册");
+              this.snackBar.open("用户名已被注册", "", {
+                duration: 1500,
+                panelClass: 'snackBar'
+              });
               break;
             case 463:
-              this.snackBar.open("用户名只支持字母,谨防信息泄露,不要使用真名");
+              this.snackBar.open("用户名只支持字母,谨防信息泄露,不要使用真名", "", {
+                duration: 1500,
+                panelClass: 'snackBar'
+              });
               break;
             case 464:
-              this.snackBar.open("邮箱已被注册");
+              this.snackBar.open("邮箱已被注册", "", {
+                duration: 1500,
+                panelClass: 'snackBar'
+              });
               break;
             case 465:
-              this.snackBar.open("当前邮箱地址不支持注册");
+              this.snackBar.open("当前邮箱地址不支持注册", "", {
+                duration: 1500,
+                panelClass: 'snackBar'
+              });
               break;
             default:
-              this.snackBar.open("注册失败,请联系管理员");
+              this.snackBar.open("注册失败,请联系管理员", "", {
+                panelClass: 'snackBar'
+              });
           }
         },
       });
