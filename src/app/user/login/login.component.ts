357eb809cef03d4d0d2bcbaff7983a00f2595b02
YaoHui <teaoea@gmail.com>
2023-01-19 14:17:12 +0800

    feat(login): login page

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {LoginService} from "./login.service";
+import {Login} from "./login";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private title: Title,
+    private service: LoginService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.title.setTitle('Login - teaoea')
+  }
+
+  login: Login = {
+    email: "",
+    password: ""
+  }
+
+  email(value: string) {
+    this.login.email = value
+  }
+
+  password(value: string) {
+    this.login.password = value
+  }
+  onSubmit() {
+    this.service.post(this.login)
+  }
 }
---
+++ src/app/user/login/login.service.ts
@@ -0,0 +1,50 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Login} from "./login";
+import url from "../../tool/http/urls.json";
+import {options} from "../../tool/http/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
+  ) {
+  }
+
+  post(login: Login) {
+    return this.http
+      .post<any>(`${url.baseUrl}${url.user.signup}`, login, options)
+      .subscribe({
+        next: (response) => {
+          localStorage.setItem("Authorization", response.body.message);
+          this.router.navigate(["/"], {relativeTo: this.route}).then();
+        },
+        error: (err) => {
+          switch (err.status) {
+            case 461 && 462:
+              this.snackBar.open('Wrong email address',
+                '', {
+                  panelClass: 'snackBar'
+                })
+              break
+            case 463:
+              this.snackBar.open('Wrong password',
+                '', {
+                  panelClass: 'snackBar'
+                })
+              break
+            default:
+              this.router.navigate(["account/login"], {skipLocationChange: true}).then();
+          }
+        }
+      })
+  }
+}
