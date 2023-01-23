1e169863d3ab0f194422a26bf43d2b0f76e6c32a
YaoHui <teaoea@gmail.com>
2023-01-23 20:47:03 +0800

    fix(username): Remove a validator

--- src/app/component/username/username.component.ts
+++ src/app/component/username/username.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { UserValidatorService } from '../../service/angular/user-validator/user-validator.service';
 
 @Component({
   selector: 'app-username',
@@ -8,36 +7,15 @@ import { UserValidatorService } from '../../service/angular/user-validator/user-
   styleUrls: ['./username.component.scss'],
 })
 export class UsernameComponent implements OnInit {
-  constructor(private userValidatorService: UserValidatorService) {}
+  value: string = '';
+  @Output() username: EventEmitter<string> = new EventEmitter();
+  formControl = new FormControl(this.value, [Validators.required]);
+
+  constructor() {}
 
   ngOnInit(): void {}
 
-  value: string = '';
-
-  @Output() username: EventEmitter<string> = new EventEmitter();
-
   newUsername(value: string) {
     this.username.emit(value);
-  }
-
-  formControl = new FormControl(this.value, [
-    Validators.required,
-    this.userInputValidator,
-  ]);
-
-  userInputValidator(formControl: FormControl): {
-    [hasError: string]: boolean;
-  } {
-    this.userValidatorService
-      .userInputValidator('username', formControl.value)
-      .subscribe({
-        next: () => {
-          return { hasError: false };
-        },
-        error: () => {
-          return { hasError: true };
-        },
-      });
-    return { hasError: false };
   }
 }
