73c470dd4e8022e41fc3cc3f775d9709f84acadd
YaoHui <teaoea@gmail.com>
2023-01-24 09:24:23 +0800

    feat(router): guard

--- src/app/router/router.module.ts
+++ src/app/router/router.module.ts
@@ -5,12 +5,13 @@ import { LoginComponent } from '../view/user/login/login.component';
 import { MeComponent } from '../view/user/me/me.component';
 import { SignupComponent } from '../view/user/signup/signup.component';
 import { NotFoundComponent } from '../view/not-found/not-found.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, runGuardsAndResolvers: 'always' },
   { path: 'account/signup', component: SignupComponent },
   { path: 'account/login', component: LoginComponent },
-  { path: 'account/info', component: MeComponent },
+  { path: 'account/info', component: MeComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }, // 404
 ];
